5058bda963585d650f441c3905483bca1812563c
olgatar <leyendaorama@gmail.com>
2016-06-27 13:18:04 +0200

    check

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 console.log('May node be with you!');
 
 const express = require('express');
-const bodyParser= require('body‐parser');
+const bodyParser = require('body‐parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, function() {
-console.log('listening on 3000')
+var db;
+MongoClient.connect('your‐mongodb‐url', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  });
 });
 
 app.get('/', function(req, res) {
-  //res.send('Hello World');
-  res.sendFile(__dirname + '/index.html');
+  res.send('Hello World');
+  //res.sendFile(__dirname + '/index.html');
 });
 
-//app.post('/quotes', (req, res) => {
-//console.log(req.body)
-//})
+app.post('/', (req, res) => {
+console.log(req.body)
+})
